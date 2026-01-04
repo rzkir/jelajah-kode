a7c28f0a006d911fe712cb7c8f4ffaca57e1099a
rzkir <rr8027896@gmail.com>
2026-01-04 17:21:37 +0700

    [update] delete discount for change productsDiscount

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,13 @@ import { Fragment } from "react";
 
 import Home from "@/components/content/home/Home";
 
-// import ProductsDiscount from "@/components/content/discount/ProductsDiscount"
+import ProductsDiscount from "@/components/content/discount/ProductsDiscount"
 
 import Products from "@/components/content/products/Products";
 
 import Services from "@/components/content/services/Services";
 
 import { fetchProducts, fetchProductsDiscount } from "@/utils/fetching/FetchProducts";
-
-import Discount from "@/components/content/discount/Discount";
 
 export default async function Page() {
   const products = await fetchProducts();
@@ -19,8 +17,7 @@ export default async function Page() {
     <Fragment>
       <Home />
       <Services />
-      <Discount productsDiscount={productsDiscount} />
-      {/* <ProductsDiscount productsDiscount={productsDiscount} /> */}
+      <ProductsDiscount productsDiscount={productsDiscount} />
       <Products products={products} />
     </Fragment>
   )
